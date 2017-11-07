26071f4c47b3ff107a441b5558c6bc4de60f8bfe
Riels <289770334@qq.com>
2017-11-07 22:27:36 +0800

    1107last version
    finish training model but cannot input to test

--- divide_word.py
+++ divide_word.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 import jieba
+import gensim.models.word2vec as w2v
 
 # coding:utf8
 # 读取倚天屠龙记文本，转码后存在新的文件里
@@ -17,6 +18,7 @@ fou.close()
 
 fin = open('倚天屠龙记_uft8.txt', 'r')
 fou = open('倚天屠龙记_segmented.txt', 'w')
+print 'finish decode'
 
 line = fin.readline()
 while line:
@@ -25,8 +27,22 @@ while line:
         .replace('“', ' ').replace('”',' ').replace('：',' ').replace('‘',' ').replace('’',' ').replace('-',' ') \
         .replace('（', ' ').replace('）',' ').replace('《',' ').replace('》',' ').replace('；',' ').replace('.',' ') \
         .replace('、', ' ').replace('…',' ')
-    print str_out
+    #print str_out
     print >> fou, newline,
     line = fin.readline()
 fin.close()
 fou.close()
+print 'finish segment'
+
+model_file_name = '倚天屠龙记_model.txt'
+# 模型训练，生成词向量
+sentences = w2v.LineSentence('倚天屠龙记_segmented.txt')
+model = w2v.Word2Vec(sentences, size=20, window=5, min_count=5, workers=4)
+
+model.save(model_file_name)
+print 'finish training'
+
+print model.similarity('赵敏'.decode('utf-8') ,'赵敏'.decode('utf-8') )
+print model.similarity('周芷若'.decode('utf-8'),'赵敏'.decode('utf-8'),)
+for k in model.similar_by_word('张三丰'.decode('utf-8')):
+    print k[0], k[1]
